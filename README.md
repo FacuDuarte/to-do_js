e096de418fce50ed4f67ffc3dae2577eec964fe4
Facundo <cailloureloaded@outlook.com>
2022-07-07 19:19:37 -0300

    Se finaliza js, css y se agrega readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Aplicación To-Do ✔️
+
+✅ Aplicación To-Do responsiva realizada con las siguentes tecnologias:
+
+- Javascript
+- Css
+- HTML
+
+---
+Utiliza la fuente [Lato](https://fonts.google.com/specimen/Lato?query=lato), que pueden ver haciendo click en el enlace, tiene la función de mostrar la fecha local.
+
+---
+![Screenshot de como se ve la página](assets/to-do_screen.png)
---
+++ index.js
@@ -0,0 +1,60 @@
+// Se seleccionan los contenedores de HTML para incluír las fechas
+const dateNumber = document.getElementById('dateNumber')
+const dateText = document.getElementById('dateText')
+const dateMonth = document.getElementById('dateMonth')
+const dateYear = document.getElementById('dateYear')
+
+// Se selecciona el div que contendrá todas las tareas
+const taskContainer = document.getElementById('taskContainer')
+
+//Función que genera la fecha actual y los muestra en los contenedores seleccionados anteriormente
+const setDate = () => {
+    const date = new Date()
+    dateNumber.textContent = date.toLocaleString('es', {day: 'numeric'})
+    dateText.textContent = date.toLocaleString('es', {weekday: 'long'})
+    dateMonth.textContent = date.toLocaleString('es', {month: 'short'})
+    dateYear.textContent = date.toLocaleString('es', {year: 'numeric'})
+}
+
+
+// Función que genera una nueva tarea, al ser un formulario, primero detiene su comportamiento por default. Guarda el valor insertado por el usuario en un div y lo guarda en el contenedor de tareas, además agrega la función changeTaskState en cada una de las tareas que se dispararán con un click
+const addNewTask = event => {
+    event.preventDefault()
+    const { value } = event.target.taskText
+    if(!value) return
+    const task = document.createElement('div')
+    task.classList.add('task', 'roundBorder')
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value
+    taskContainer.prepend(task)
+    event.target.reset()   
+}
+
+
+// Función dada a cada tarea que activa o desactiva la clase css done
+const changeTaskState = event => {
+    event.target.classList.toggle('done')
+}
+
+//Función que crea dos array. uno con las tareas hechas y otro con las tareas por hacer. Itera los hijos del contenedor con las tareas y en base a si contienen la clase done los agrupa en uno de los arrays creados, luego retorna una copia de esos arrays, primero el de tareas por hacer y luego el de hechas
+const order = () => {
+    const done = []
+    const toDo = []
+    taskContainer.childNodes.forEach( el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el)
+    })
+    return [...toDo, ...done]
+}
+
+
+// Función que llama a la funcion order, itera el array devuelto y crea un hijo en el contenedor de tareas por cada item del array
+const renderOrderedTask = () => {
+    order().forEach( el => taskContainer.appendChild(el))
+}
+
+
+// Función que genera una nueva fecha, se actualiza cada vez que se carga el documento
+setDate()
+
+
+// Funcionalidades extra, se puede eliminar definitivamente una tarea, se pueden order por completadas o por hacer y se pueden arrastrar tareas.
